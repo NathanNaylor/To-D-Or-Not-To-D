3493276a8481a2f54c37b4b4aaed3a9fbb1952d5
Squelchert <61394430+NathanNaylor@users.noreply.github.com>
2020-04-23 14:47:31 -0700

    drawing character data from table

--- public/js/character.js
+++ public/js/character.js
@@ -2,6 +2,20 @@ $(document).ready(function() {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(function(data) {
-    $(".character-name").text(data.email);
+    // console.log(data);
+
+    getCharacters(data.id);
   });
+
+  // getCharacters();
+
+  function getCharacters(userID) {
+    $.get("/api/user_data/" + userID, function(data) {
+      console.log(data);
+      $(".character-name").text(data[0].Characters[0].name);
+      $(".character-class").text(data[0].Characters[0].characterClass);
+      $(".character-race").text(data[0].Characters[0].race);
+      $(".character-alignment").text(data[0].Characters[0].alignment);
+    });
+  }
 });
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -39,8 +39,8 @@ module.exports = function(app) {
   // Route for getting some data about our user to be used client side
   app.get("/api/user_data", function(req, res) {
     if (!req.user) {
-      // The user is not logged in, send back an empty object
-      res.json({});
+      res.sendStatus(403);
+      return;
     } else {
       // Otherwise send back the user's email and id
       // Sending back a password, even a hashed password, isn't a good idea
@@ -81,4 +81,20 @@ module.exports = function(app) {
   //     console.log(res);
   //   }
   // });
+
+  app.get("/api/user_data/:id", function(req, res) {
+    if (!req.user) {
+      res.sendStatus(403);
+      return;
+    } else {
+      db.User.findAll({
+        where: {
+          id: req.params.id
+        },
+        include: db.Character
+      }).then(data => {
+        res.json(data);
+      });
+    }
+  });
 };
